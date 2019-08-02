4d51996b4ce32bfa6908bd9b6171eeb7c6271803
Alperen Balık <a.balik@tr.cervel.systems>
2019-08-02 16:26:34 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='hptpy',
+      version='0.0.1',
+      packages=["hptpy"],
+      author="Alperen Balık",
+      author_email="alperenbalik@outlook.com",
+      description="Hypothesis Testing Python Library",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/alperenbalik/hptpy",
+      install_requires=['scipy>=0.9.0',
+                        'numpy==1.15.4',
+                        'statsmodels>=0.9.0']
+      )
